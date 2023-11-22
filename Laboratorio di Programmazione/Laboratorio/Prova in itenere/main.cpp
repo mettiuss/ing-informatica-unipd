690974073b61fc302548c194a48ffbe1c148a97f
Mettius <69057048+MettiusHyper@users.noreply.github.com>
2023-11-22 17:34:07 +0100

    Update test

--- Laboratorio di Programmazione/Laboratorio/Prova in itenere/main.cpp
+++ Laboratorio di Programmazione/Laboratorio/Prova in itenere/main.cpp
@@ -5,13 +5,13 @@
 // Date class tester
 void test_date() {
   try {
-    Date d1(2023, Date::jun, 31); // invalid day
+    Date d1(2023, Date::jun, 31);  // invalid day
   } catch (Date::Invalid) {
     std::cout << "Invalid date caught by day" << std::endl;
   }
 
   try {
-    Date d2(2024, Date::feb, 29); // leap year check
+    Date d2(2024, Date::feb, 29);  // leap year check
   } catch (Date::Invalid) {
     std::cout << "Invalid date caught by leap year" << std::endl;
   }
@@ -19,7 +19,7 @@ void test_date() {
   Date d3;
   std::cout << d3 << std::endl;
 
-  Date d4(1950, Date::dec, 30); // nascita di Bjarne Stroustrup
+  Date d4(1950, Date::dec, 30);  // nascita di Bjarne Stroustrup
   std::cout << d4 << std::endl;
 }
 
@@ -43,31 +43,21 @@ void test_book() {
 }
 
 void test_bookshelf() {
-  Date invalidDate(2008, Date::feb, 29);
-  Date copyright(2023, Date::nov, 22);
-  Date d2(1950, Date::dec, 30);  // nascita di Bjarne Stroustrup
+  Date d(1950, Date::dec, 30);  // nascita di Bjarne Stroustrup
 
   BookShelf b = BookShelf();
-  Book book1("Francesca", "Rossi", "Title1", d2, "__CODICEISBN1");
+  Book book1("Francesca", "Rossi", "Title1", d, "__CODICEISBN1");
   Book book2("Giulio", "Rossi", "Title1", "__CODICEISBN1");
-  Book book3("Pietro", "Rossi", "Title2", invalidDate, "__CODICEISBN2");
-
-  book2.registerCopyright(copyright);
-  book3.registerCopyright(d2);
 
   b.push_back(book1);
   b.push_back(book2);
 
   std::cout << "Posizione 1: " << b.safe_get(0) << std::endl;
-  std::cout << "Posizione 2: " << b[1] << std::endl << std::endl;
+  std::cout << "Posizione 2: " << b[1] << std::endl;
 
   std::cout << "Risetto il libro nella seconda posizione..." << std::endl;
-  b.safe_set(1, book3);
-  b[1].rentBook();
+  b.safe_set(1, book1);
   std::cout << "Posizione 2: " << b[1] << std::endl << std::endl;
-  b[1].rentBook();
-  b[1].returnBook();
-  b[1].returnBook();
 }
 
 int main() {
