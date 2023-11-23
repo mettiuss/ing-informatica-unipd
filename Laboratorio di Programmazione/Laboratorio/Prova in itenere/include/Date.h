f19c558f2c51cc4c1402044886d05cbbfda768dc
Mettius <69057048+MettiusHyper@users.noreply.github.com>
2023-11-23 11:56:08 +0100

    🎨 add comments

--- Laboratorio di Programmazione/Laboratorio/Prova in itenere/include/Date.h
+++ Laboratorio di Programmazione/Laboratorio/Prova in itenere/include/Date.h
@@ -16,23 +16,21 @@ class Date {
   class Invalid {};
 
   enum Month { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
-  
-  //===== Costruttori =====//
-    /* Costruttore di default, la data di default è 01/01/0000*/
-    Date() : y{0}, m{jan}, d{1} {}
 
-    /* Costruttore con parametri, setta anno/mese/giorno */
-    Date(int yy, Month mm, int dd);
-  //=======================//
-  
-  //===== Funzioni get (const) =====//
-    /* Funzioni che permettono di ottenere le variabili membro private
-       Le seguenti funzioni sono const => non possono in alcun modo
-       modificare lo stato dell'oggetto Book */
-    Month getMonth() const { return m; }
-    int getDay() const { return d; }
-    int getYear() const { return y; }
-  //================================//
+  //============= Costruttori ======================
+  /* Costruttore di default, la data di default è 01/01/0000*/
+  Date() : y{0}, m{jan}, d{1} {}
+
+  /* Costruttore con parametri, setta anno/mese/giorno */
+  Date(int yy, Month mm, int dd);
+
+  //============= Funzioni get (const) =============
+  /* Funzioni che permettono di ottenere le variabili membro private
+     Le seguenti funzioni sono const => non possono in alcun modo
+     modificare lo stato dell'oggetto Book */
+  Month getMonth() const { return m; }
+  int getDay() const { return d; }
+  int getYear() const { return y; }
 
  private:
   int y, d;
@@ -40,17 +38,17 @@ class Date {
 };
 
 //===== Overload operatori =====//
-  /* Overload operatore di confronto uguaglianza
-     ritorna true <=> le due date sono uguali */
-  bool operator==(Date firstDate, Date secondDate);
-  
-  /* Overload operatore di out stream
-     permette di stampare a stream l'oggetto Date */
-  std::ostream &operator<<(std::ostream &stream, Date operand);
+/* Overload operatore di confronto uguaglianza
+   ritorna true <=> le due date sono uguali */
+bool operator==(Date firstDate, Date secondDate);
+
+/* Overload operatore di out stream
+   permette di stampare a stream l'oggetto Date */
+std::ostream &operator<<(std::ostream &stream, Date operand);
 //==============================//
 
 //===== LeapYear =====//
-  /* Funzione che ritorna una valore booleano a seconda
-     se l'anno è bisestelo o no. True <=> l'anno è bisestile*/
-  bool isLeapYear(int year);
+/* Funzione che ritorna una valore booleano a seconda
+   se l'anno è bisestelo o no. True <=> l'anno è bisestile*/
+bool isLeapYear(int year);
 //====================//
