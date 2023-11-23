1ce652f58acf43154070c052674231b499ac885e
mattiameneguolo <mattiameneguolo@gmail.com>
2023-11-23 11:43:00 +0100

    [+] Add: Commenti header Date

--- Laboratorio di Programmazione/Laboratorio/Prova in itenere/include/Book.h
+++ Laboratorio di Programmazione/Laboratorio/Prova in itenere/include/Book.h
@@ -24,71 +24,71 @@ class Book {
   enum Status { Disponibile = true, Fuori = false };
 
   //===== Costruttori =====//
-  /* Costruttore di default, i membri string vengono settati come stringhe
-     vuote, mentre la data di copyrigth viene settata a 01/01/0000 */
-  Book()
-      : authorName{""},
-        authorSurname{""},
-        bookTitle{""},
-        copyrightDate{nullDate},
-        bookISBN{""} {}
+     /* Costruttore di default, i membri string vengono settati come stringhe
+          vuote, mentre la data di copyrigth viene settata a 01/01/0000 */
+     Book()
+          : authorName{""},
+          authorSurname{""},
+          bookTitle{""},
+          copyrightDate{nullDate},
+          bookISBN{""} {}
 
-  /* Costruttore per Book non coperti da copyright, viene settato tutto tranne
-     tranne la data, che viene settata a 01/01/0000 */
-  Book(std::string aName, std::string aSurname, std::string title,
-       std::string ISBN);
+     /* Costruttore per Book non coperti da copyright, viene settato tutto tranne
+          tranne la data, che viene settata a 01/01/0000 */
+     Book(std::string aName, std::string aSurname, std::string title,
+          std::string ISBN);
 
-  /* Costruttore per Book protetti da copyright, viene settato ogni dato */
-  Book(std::string aName, std::string aSurname, std::string title, Date data,
-       std::string ISBN);
+     /* Costruttore per Book protetti da copyright, viene settato ogni dato */
+     Book(std::string aName, std::string aSurname, std::string title, Date data,
+          std::string ISBN);
   //=======================//
 
   //===== Funzioni get (const) =====//
-  /* Funzioni che permettono di ottenere le variabili membro private
-     Le seguenti funzioni sono const => non possono in alcun modo
-     modificare lo stato dell'oggetto Book */
+     /* Funzioni che permettono di ottenere le variabili membro private
+          Le seguenti funzioni sono const => non possono in alcun modo
+          modificare lo stato dell'oggetto Book */
 
-  /* getAuthor -> ritorna una string contenente la concatenazione di nome e
-   * cognome dell'autore */
-  std::string getAuthor() const { return authorName + " " + authorSurname; }
+     /* getAuthor -> ritorna una string contenente la concatenazione di nome e
+     * cognome dell'autore */
+     std::string getAuthor() const { return authorName + " " + authorSurname; }
 
-  /* getAuthorName -> ritorna una string contenente il nome dell'autore del
-   * libro*/
-  std::string getAuthorName() const { return authorName; }
+     /* getAuthorName -> ritorna una string contenente il nome dell'autore del
+     * libro*/
+     std::string getAuthorName() const { return authorName; }
 
-  /* getAuthorSurame -> ritorna una string contenente il cognome dell'autore del
-   * libro*/
-  std::string getAuthorSurname() const { return authorSurname; }
+     /* getAuthorSurame -> ritorna una string contenente il cognome dell'autore del
+     * libro*/
+     std::string getAuthorSurname() const { return authorSurname; }
 
-  /* getTitle -> ritorna una string contenente il titolo del libro*/
-  std::string getTitle() const { return bookTitle; }
+     /* getTitle -> ritorna una string contenente il titolo del libro*/
+     std::string getTitle() const { return bookTitle; }
 
-  /* getCopyrightDate -> ritorna un Date contenente la data di registrazione del
-   * copyright */
-  Date getCopyrightDate() const { return copyrightDate; }
+     /* getCopyrightDate -> ritorna un Date contenente la data di registrazione del
+     * copyright */
+     Date getCopyrightDate() const { return copyrightDate; }
 
-  /* getIsbn -> ritorna una string contenente l'ISBN del libro*/
-  std::string getIsbn() const { return bookISBN; }
+     /* getIsbn -> ritorna una string contenente l'ISBN del libro*/
+     std::string getIsbn() const { return bookISBN; }
 
-  /* getBookStatus -> ritorna uno Status contenente lo stato del libro
-     (Disponibile/Fuori) */
-  Status bookStatus() const { return status; }
+     /* getBookStatus -> ritorna uno Status contenente lo stato del libro
+          (Disponibile/Fuori) */
+     Status bookStatus() const { return status; }
   //==============================//
 
   //===== Registrazione copyright =====//
-  /* Funzione che permette la registrazione del copyright
-     in seguito alla creazione del libro */
-  void registerCopyright(Date data);
-  //===================================//
+     /* Funzione che permette la registrazione del copyright
+          in seguito alla creazione del libro */
+     void registerCopyright(Date data);
+     //===================================//
 
-  //===== Gestione prestito =====//
-  /* Funzione che permette il prestito del libro, se il libro
-     è già stato prestato, assicurarsi che sia rientrato prima di riprestarlo*/
-  void rentBook();
+     //===== Gestione prestito =====//
+     /* Funzione che permette il prestito del libro, se il libro
+          è già stato prestato, assicurarsi che sia rientrato prima di riprestarlo*/
+     void rentBook();
 
-  /* Funzione che permette la restituzione del libro, se il libro
-     non è in prestito, non fa nulla */
-  void returnBook();
+     /* Funzione che permette la restituzione del libro, se il libro
+          non è in prestito, non fa nulla */
+     void returnBook();
   //=============================//
 
  private:
@@ -98,15 +98,15 @@ class Book {
 };
 
 //===== Overload operatori =====//
-/* Overload operatore di confronto uguaglianza
-   ritorna true <=> l'ISBN dei due Book è uguale */
-bool operator==(Book firstBook, Book secondBook);
+  /* Overload operatore di confronto uguaglianza
+     ritorna true <=> l'ISBN dei due Book è uguale */
+  bool operator==(Book firstBook, Book secondBook);
 
-/* Overload operatore di confronto diversità
-   ritorna true <=> l'ISBN dei due Book è diverso */
-bool operator!=(Book firstBook, Book secondBook);
+  /* Overload operatore di confronto diversità
+     ritorna true <=> l'ISBN dei due Book è diverso */
+  bool operator!=(Book firstBook, Book secondBook);
 
-/* Overload operatore di out stream
-   permette di stampare a stream l'oggetto Book */
-std::ostream &operator<<(std::ostream &os, Book book);
+  /* Overload operatore di out stream
+     permette di stampare a stream l'oggetto Book */
+  std::ostream &operator<<(std::ostream &os, Book book);
 //==============================//
--- Laboratorio di Programmazione/Laboratorio/Prova in itenere/include/Date.h
+++ Laboratorio di Programmazione/Laboratorio/Prova in itenere/include/Date.h
@@ -1,20 +1,53 @@
+/**
+ * Date Class
+ * Copyright (c) 2023 group: Laboratorio della Latrina
+ *
+ * Questa classe permette la creazione di oggetti di tipo Date
+ * Ogni Date deve contenere diverse informazioni, quali:
+ *  -Giorno
+ *  -Mese
+ *  -Anno
+ */
+
 #include <iostream>
 
 class Date {
  public:
   class Invalid {};
+
   enum Month { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec };
-  Date() : y{0}, m{jan}, d{1} {}
-  Date(int yy, Month mm, int dd);
-  Month getMonth() const { return m; }
-  int getDay() const { return d; }
-  int getYear() const { return y; }
+  
+  //===== Costruttori =====//
+    Date() : y{0}, m{jan}, d{1} {}
+    Date(int yy, Month mm, int dd);
+  //=======================//
+  
+  //===== Funzioni get (const) =====//
+    /* Funzioni che permettono di ottenere le variabili membro private
+       Le seguenti funzioni sono const => non possono in alcun modo
+       modificare lo stato dell'oggetto Book */
+    Month getMonth() const { return m; }
+    int getDay() const { return d; }
+    int getYear() const { return y; }
+  //================================//
 
  private:
   int y, d;
   Month m;
 };
 
-bool operator==(Date firstDate, Date secondDate);
-std::ostream &operator<<(std::ostream &stream, Date operand);
-bool isLeapYear(int year);
+//===== Overload operatori =====//
+  /* Overload operatore di confronto uguaglianza
+     ritorna true <=> le due date sono uguali */
+  bool operator==(Date firstDate, Date secondDate);
+  
+  /* Overload operatore di out stream
+     permette di stampare a stream l'oggetto Date */
+  std::ostream &operator<<(std::ostream &stream, Date operand);
+//==============================//
+
+//===== LeapYear =====//
+  /* Funzione che ritorna una valore booleano a seconda
+     se l'anno è bisestelo o no. True <=> l'anno è bisestile*/
+  bool isLeapYear(int year);
+//====================//
