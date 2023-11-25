3125259c1916ff9ab61b4d002cf19b37dad194a8
mattiameneguolo <mattiameneguolo@gmail.com>
2023-11-25 19:02:48 +0100

    [+] Add: commenti Bookshelf.cpp e Date.cpp

--- Laboratorio di Programmazione/Laboratorio/prova-in-itinere/src/BookShelf.cpp
+++ Laboratorio di Programmazione/Laboratorio/prova-in-itinere/src/BookShelf.cpp
@@ -2,12 +2,14 @@
 
 #include <iostream>
 
+// Costruttore default dell'oggetto Bookshelf [su vettore di 10 elementi]
 BookShelf::BookShelf() {
   size = 0;
   max_size = 10;
   b = new Book[max_size];
 }
 
+// Costruttore con dimensione vettore
 BookShelf::BookShelf(int dimension) {
   if (dimension <= 0) throw Invalid();
   size = 0;
@@ -15,29 +17,36 @@ BookShelf::BookShelf(int dimension) {
   b = new Book[dimension];
 }
 
+// Distruttore di Bookshelf
 BookShelf::~BookShelf() { delete[] b; }
 
+// Set elemento, con controllo validità indice
 void BookShelf::safe_set(int index, Book value) {
   if (index < 0 || index >= size) throw Invalid();
   b[index] = value;
 }
 
+// Get elemento, con controllo validità indice
 Book BookShelf::safe_get(int index) const {
   if (index < 0 || index >= size) throw Invalid();
   return b[index];
 }
 
+// Get reference di un elemento, con controllo validità indice
 Book &BookShelf::at(int index) {
   if (index < 0 || index >= size) throw Invalid();
   return b[index];
 }
 
+// Get costante della reference di un elemento, con controllo validità indice
 const Book &BookShelf::at(int index) const {
   if (index < 0 || index >= size) throw Invalid();
   return b[index];
 }
 
+// Funzione per aggiunta elementi in coda al vettore Bookshelf
 void BookShelf::push_back(Book value) {
+  // Resize vettore nel caso in cui sia pieno
   if (max_size == 0) {
     resize(10);
   } else if (size == max_size) {
@@ -48,33 +57,38 @@ void BookShelf::push_back(Book value) {
   size++;
 }
 
+// Get + Rimozione dell'ultimo elemento del vettore Bookshelf
 Book BookShelf::pop_back() {
   size--;
   return b[size];
 }
 
+// Overload operatore [], per get e set tramite offset dall'indirizzo di b
 Book &BookShelf::operator[](int index) { return b[index]; }
 
+// Overload operatore [] const, per solo get tramite offset dall'indirizzo di b
 Book BookShelf::operator[](int index) const { return b[index]; }
 
+// Funzione per riserva spazio del vettore Bookshelf
 void BookShelf::reserve(int dimension) {
+  /* Lo spazio viene riservato solo se la nuova dimensione è
+  maggiore della dimensione attuale del vettore */
   if (dimension > max_size) resize(dimension);
+  
   reserved = dimension;
 }
 
+// Ridimensionamento del vettore Bookshelf
 void BookShelf::resize(int dimension) {
   Book *newArray;
 
-  if (dimension > reserved) {
-    newArray = new Book[dimension];
-    max_size = dimension;
-  } else {
-    newArray = new Book[reserved];
-    max_size = reserved;
-  }
+  newArray = new Book[dimension];
+  max_size = dimension;
 
+  // Copia del vecchio vettore Bookshelf in quello nuovo
   std::copy(b, b + size, newArray);
 
+  // Liberazione memoria riservata al vecchio vettore Bookshelf
   delete[] b;
 
   b = newArray;
--- Laboratorio di Programmazione/Laboratorio/prova-in-itinere/src/Date.cpp
+++ Laboratorio di Programmazione/Laboratorio/prova-in-itinere/src/Date.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+// Costruttore dell'oggetto Date
 Date::Date(int yy, Month mm, int dd) {
   // Controlli di base
   if (mm < 1 || mm > 12) throw Invalid();
@@ -10,7 +11,7 @@ Date::Date(int yy, Month mm, int dd) {
   // Aprile, Giugno, Settembre e Novembre hanno 30 giorni
   if (dd == 31 && (mm == 4 || mm == 6 || mm == 9 || mm == 11)) throw Invalid();
 
-  // Controllo su Febbraio
+  // Controllo su Febbraio, se l'anno è bisestile ha 29, se no 28
   if (mm == 2 && ((dd == 29 && !isLeapYear(yy)) || dd > 29)) throw Invalid();
 
   y = yy;
@@ -18,6 +19,7 @@ Date::Date(int yy, Month mm, int dd) {
   d = dd;
 }
 
+// Controllo anno bisesitile
 bool isLeapYear(int year) {
   if (year % 400 == 0)
     return true;
@@ -28,10 +30,12 @@ bool isLeapYear(int year) {
   return false;
 }
 
+// Overload operatore di confronto (uguaglianza)
 bool operator==(Date firstDate, Date secondDate){
   return (firstDate.getDay() == secondDate.getDay() && firstDate.getMonth() == secondDate.getMonth() && firstDate.getYear() == secondDate.getYear());
 }
 
+// Overload operatore di print a stream
 std::ostream &operator<<(std::ostream &stream, Date operand) {
   return stream << operand.getDay() << "/" << operand.getMonth() << "/"
                 << operand.getYear();
